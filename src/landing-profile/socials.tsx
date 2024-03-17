e023f93700c4ed32dbe5e0cae4d205fd2700d856
Mikhail Vedernikov <misha.smert@gmail.com>
2024-03-17 09:20:58 +0200

    feat: added posthog for fun & learning the product

--- src/landing-profile/socials.tsx
+++ src/landing-profile/socials.tsx
@@ -1,5 +1,6 @@
 import { mdiEmail, mdiGithub, mdiLinkedin, mdiNpm, mdiTwitch, mdiTwitter } from '@mdi/js';
 import Icon from '@mdi/react';
+import posthog from 'posthog-js';
 import React from 'react';
 
 type TIconLink = {
@@ -10,12 +11,12 @@ type TIconLink = {
 };
 
 const iconLinks: Array<TIconLink> = [
-  { className: 'hover:fill-[#615d5d] active:fill-[#615d5d]', icon: mdiGithub, url: 'https://github.com/Smertos' },
-  { className: 'hover:fill-[#e39e44] active:fill-[#e39e44]', icon: mdiEmail, url: 'mailto:mr.smertos@gmail.com', noBlank: true },
-  { className: 'hover:fill-[#0077b5] active:fill-[#0077b5]', icon: mdiLinkedin, url: 'https://www.linkedin.com/in/smertos/' },
-  { className: 'hover:fill-[#cb0000] active:fill-[#cb0000]', icon: mdiNpm, url: 'https://www.npmjs.com/~smertos' },
-  { className: 'hover:fill-[#1da1f2] active:fill-[#1da1f2]', icon: mdiTwitter, url: 'https://twitter.com/Smertos' },
-  { className: 'hover:fill-[blueviolet] active:fill-[blueviolet]', icon: mdiTwitch, url: 'https://www.twitch.tv/smertos' }
+  { className: 'hover:text-[#615d5d] active:text-[#615d5d]', icon: mdiGithub, url: 'https://github.com/Smertos' },
+  { className: 'hover:text-[#e39e44] active:text-[#e39e44]', icon: mdiEmail, url: 'mailto:mr.smertos@gmail.com', noBlank: true },
+  { className: 'hover:text-[#0077b5] active:text-[#0077b5]', icon: mdiLinkedin, url: 'https://www.linkedin.com/in/smertos/' },
+  { className: 'hover:text-[#cb0000] active:text-[#cb0000]', icon: mdiNpm, url: 'https://www.npmjs.com/~smertos' },
+  { className: 'hover:text-[#1da1f2] active:text-[#1da1f2]', icon: mdiTwitter, url: 'https://twitter.com/Smertos' },
+  { className: 'hover:text-[blueviolet] active:text-[blueviolet]', icon: mdiTwitch, url: 'https://www.twitch.tv/smertos' }
 ];
 
 export function LandingProfileSocials() {
@@ -23,8 +24,12 @@ export function LandingProfileSocials() {
     <div className="inline-flex flex-wrap gap-2 mt-4 mx-auto sm:mt-3 sm:mx-0">
       {iconLinks.map((iconLink) => (
         <a
-          className={'h-[1.875rem] w-[1.875rem] p-1 bg-transparent no-underline cursor-pointer ' + iconLink.className}
+          className={'h-[1.875rem] w-[1.875rem] bg-transparent no-underline cursor-pointer ' + iconLink.className}
+          href={iconLink.url}
           key={btoa(encodeURIComponent(iconLink.url))}
+          onClick={() => posthog.capture('socials_click', { hrefUrl: iconLink.url })}
+          rel="noopener noreferrer"
+          target={iconLink.noBlank ? '' : '_blank'}
         >
           <Icon path={iconLink.icon} size={1.25} />
         </a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import posthog from 'posthog-js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './index.css';
+
+try {
+  posthog.init(import.meta.env.VITE_POSTHOG_TOKEN, { api_host: 'https://eu.posthog.com', respect_dnt: true,  })
+} catch (error) {
+  console.error('Failed to initialize PostHog', error);
+}
+
+console.debug('import.meta.env', import.meta.env);
 
 const rootElement = document.getElementById('root');
 
